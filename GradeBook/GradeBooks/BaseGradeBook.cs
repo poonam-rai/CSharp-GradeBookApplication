033889979b539eee5a43754b501f7a05e8a0f8d0
poonam-rai <58810919+poonam-rai@users.noreply.github.com>
2020-03-28 14:49:58 +0100

    xx

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -15,10 +15,11 @@ namespace GradeBook.GradeBooks
         public List<Student> Students { get; set; }
         public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name, bool IsWeighted)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public GradeBookType Type { get; set; }
