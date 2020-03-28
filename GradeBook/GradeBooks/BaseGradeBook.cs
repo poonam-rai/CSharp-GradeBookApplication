97f7b740541bcd815937edbe91999ca9eadd14db
poonam-rai <58810919+poonam-rai@users.noreply.github.com>
2020-03-28 15:45:21 +0100

    added isweighted property in basegradebook

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,7 +13,7 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
-        //public bool IsWeighted { get; set; }
+        public bool IsWeighted { get; set; }
 
         public BaseGradeBook(string name)
         {
