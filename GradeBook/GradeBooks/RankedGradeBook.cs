69fa62976a44dd717c9649db535ccde273c5d1c4
poonam-rai <58810919+poonam-rai@users.noreply.github.com>
2020-03-28 14:51:18 +0100

    Revert "second part"
    This reverts commit 92133ee5cd64300b220f9e1552062cb7daa6b789.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -57,18 +57,6 @@ namespace GradeBook.GradeBooks
                 base.CalculateStatistics();
             }
         }
-
-        public override void CalculateStudentStatistics(string name)
-        {
-            if (Students.Count() < 5)
-            {
-                Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-               return;
-            }
-            else {
-                base.CalculateStudentStatistics(name);
-            }
-        }
     }
 }
 
